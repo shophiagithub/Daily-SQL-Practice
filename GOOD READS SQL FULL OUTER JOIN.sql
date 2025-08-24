e0c506cc5d8947dc6c9d9fc3277fd0a1cc492b97
Shophia <114874837+shophiagithub@users.noreply.github.com>
2025-08-24 23:03:28 +0530

    Create GOOD READS SQL FULL OUTER JOIN.sql

---
+++ GOOD READS SQL FULL OUTER JOIN.sql
@@ -0,0 +1,19 @@
+SELECT 
+  o.order_id, 
+  d.delivery_id, 
+  d.delivery_date, 
+  d.delivery_status
+FROM orders o
+LEFT JOIN deliveries d
+  ON o.order_id = d.order_id
+
+UNION
+
+SELECT 
+  o.order_id, 
+  d.delivery_id, 
+  d.delivery_date, 
+  d.delivery_status
+FROM orders o
+RIGHT JOIN deliveries d
+  ON o.order_id = d.order_id;
