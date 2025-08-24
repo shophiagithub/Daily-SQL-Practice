ad565fe0b801885716b68fe6c07fce00bcc93fbd
Shophia <114874837+shophiagithub@users.noreply.github.com>
2025-08-24 12:21:23 +0530

    Create Counting Results using SQL COUNT() in CASE Statement.sql

---
+++ Counting Results using SQL COUNT() in CASE Statement.sql
@@ -0,0 +1,12 @@
+SELECT
+  platform,
+  COUNT(CASE 
+    WHEN followers >= 500000 THEN 1
+    ELSE NULL
+  END) AS popular_actor_count,
+  COUNT(CASE 
+    WHEN followers < 500000 THEN 1
+    ELSE NULL
+  END) AS less_popular_actor_count
+FROM marvel_avengers
+GROUP BY platform;
